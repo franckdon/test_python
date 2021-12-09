f5b2a4f96fefb2c252fcabb5c5d3a3bea78b6045
franckdon <frk1990@hotmail.com>
2021-12-09 20:32:33 +0000

    sousctraction

--- vector.py
+++ vector.py
@@ -7,3 +7,21 @@ class Vector:
     def norme(self):
         x = math.sqrt(self.x**2 + self.y**2)
         return x
+    
+    def somme(self, t):
+        self.x += t.x
+        self.y += t.y
+
+    def sous(self, t):
+        self.x -= t.x
+        self.y -= t.y
+
+v1 = Vector(4,3)
+v2 = Vector(3,4)
+v1.somme(v2)
+print(v1.x)
+print(v1.y)
+
+v1.sous(v2)
+print(v1.x)
+print(v1.y)
